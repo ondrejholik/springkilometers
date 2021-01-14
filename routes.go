66cee30e02c8ed6cc808105f63c225bc28cfaa18
Ondřej Holík <andyholik2@gmail.com>
2021-01-14 20:08:52 +0100

    removing article subcategory

--- routes.go
+++ routes.go
@@ -37,22 +37,6 @@ func initializeRoutes() {
 		userRoutes.POST("/register", ensureNotLoggedIn(), register)
 	}
 
-	// Group article related routes together
-	articleRoutes := router.Group("/article")
-	{
-		// Handle GET requests at /article/view/some_article_id
-		articleRoutes.GET("/view/:article_id", getArticle)
-
-		// Handle the GET requests at /article/create
-		// Show the article creation page
-		// Ensure that the user is logged in by using the middleware
-		articleRoutes.GET("/create", ensureLoggedIn(), showArticleCreationPage)
-
-		// Handle POST requests at /article/create
-		// Ensure that the user is logged in by using the middleware
-		articleRoutes.POST("/create", ensureLoggedIn(), createArticle)
-	}
-
 	tripRoutes := router.Group("/trip")
 	{
 		tripRoutes.GET("/view/:trip_id", getTrip)
