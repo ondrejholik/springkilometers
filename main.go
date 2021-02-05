57fee34ca12ccfcf645bf318cb5e749d6ddfb67a
Ondřej Holík <andyholik2@gmail.com>
2021-02-05 12:11:52 +0100

    log database loggin

--- main.go
+++ main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	models "github.com/ondrejholik/springkilometers/models"
+  "log"
 )
 
 var router *gin.Engine
 
 func main() {
 
+	//err := godotenv.Load()
+	//if err != nil {
+	//log.Panic(err)
+	//}
+
+	//dburl := os.Getenv("DATABASE_URL")
+	//user := os.Getenv("USERNAME")
+	//dbname := os.Getenv("DATABASE")
+	//dbpass := os.Getenv("PASSWORD")
+	//dbport := os.Getenv("PORT")
+	//dbhostname := os.Getenv("HOST")
+
+  log.Println("Connecting to database")
 	models.Setup()
+  log.Println("Connected")
 	// Set Gin to production mode
 	gin.SetMode(gin.ReleaseMode)
 
